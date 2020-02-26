27a4732d252b88057223d1f88a0a6e3c79688be5
tomkf <thomaskeithferris@gmail.com>
2020-02-26 15:03:09 -0800

    item component

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, FlatList, SafeAreaView } from 'react-native';        
+import RecipeItem from './components/RecipeItem'
 
 export default function App() {
+  const recipes = [
+    { title: "Best Brownies", steps: ["step1", "step2"], imageURL: "https://images.pexels.com/photos/45202/brownie-dessert-cake-sweet-45202.jpeg" },
+    { title: "Banana Bread", steps: ["step1", "step2"], imageURL: "https://images.pexels.com/photos/830894/pexels-photo-830894.jpeg" },
+    { title: "Chocolate Chip Cookies", steps: ["step1", "step2"], imageURL: "https://images.pexels.com/photos/230325/pexels-photo-230325.jpeg" }
+  ]
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        keyExtractor={recipe => recipe.title}
+        data={recipes}
+        renderItem={({ item }) => {
+          return <RecipeItem recipe={item} />
+      }} />
+    </SafeAreaView>
+);
 }
 
 const styles = StyleSheet.create({
